037387bdc4efdcb17e64f7b2667fae00468aa1b3
KarimAlii <kariim.alii1@gmail.com>
2024-01-02 19:05:36 +0200

    Clean up the package.

--- src/FilamentLocalLoginsServiceProvider.php
+++ src/FilamentLocalLoginsServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace BetterFuturesStudio\FilamentLocalLogins;
 
-use BetterFuturesStudio\FilamentLocalLogins\Commands\FilamentLocalLoginsCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,15 +9,9 @@ class FilamentLocalLoginsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('filament-local-logins')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasCommand(FilamentLocalLoginsCommand::class);
+            ->hasViews();
     }
 }
